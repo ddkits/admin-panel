923d388774263a8a2477250f835be3ed4b3153fc
Sam Elayyoub <m22386@live.com>
2022-02-19 16:34:38 -0800

    Clean version of Database seed

---
+++ src/Database/Migrations/2017_08_29_030005_create_post_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('post', function (Blueprint $table) {
+            $table->increments('id');
+            $table->bigInteger('uid')->unsigned();
+            $table->string('title');
+            $table->longText('body');
+            $table->string('path');
+            $table->string('tags')->default('N/A');
+            $table->timestamps();
+
+            $table->foreign('uid')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('post');
+    }
+}
---
+++ src/Database/Migrations/2018_05_01_040410_create_menus_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMenusTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('menus', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('weight')->default(0);
+            $table->string('name');
+            $table->string('menu')->default('mainmenu');
+            $table->integer('menuparent')->nullable();
+            $table->integer('children')->default(0);
+            $table->text('description')->nullable();
+            $table->bigInteger('plan_id')->nullable();
+            $table->string('link');
+            $table->string('class')->nullable();
+            $table->string('iconclass')->nullable();
+            $table->string('adminlevel')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('menus');
+    }
+}
