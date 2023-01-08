83d73efe0bbfd34435cabd1e2484cf50a1d1a83d
Sam Elayyoub <m22386@live.com>
2023-01-08 00:58:54 -0800

    Update 2017_08_29_030005_create_post_table.php

--- src/Database/Migrations/2017_08_29_030005_create_post_table.php
+++ src/Database/Migrations/2017_08_29_030005_create_post_table.php
@@ -13,17 +13,77 @@ class CreatePostTable extends Migration
      */
     public function up()
     {
-        Schema::create('post', function (Blueprint $table) {
-            $table->increments('id');
-            $table->bigInteger('uid')->unsigned();
-            $table->string('title');
-            $table->longText('body');
-            $table->string('path');
-            $table->string('tags')->default('N/A');
-            $table->timestamps();
+        
+        if (!Schema::hasTable('tblCategory')) {
+            Schema::create('post', function (Blueprint $table) {
+                $table->increments('id');
+                $table->bigInteger('uid')->unsigned();
+                $table->longText('title');
+                $table->longText('body');
+                $table->string('path');
+                $table->string('tags')->default('N/A');
+                $table->timestamps();
 
-            $table->foreign('uid')->references('id')->on('users');
-        });
+                $table->foreign('uid')->references('id')->on('users');
+            });
+            
+        }else{
+            if (Schema::hasColumn('post', 'uid')){
+            return;
+            }else{
+                Schema::table('post', function (Blueprint $table)
+                {
+                    $table->bigInteger('uid')->unsigned();
+                    $table->foreign('uid')->references('id')->on('users');
+                });
+            }
+            if (Schema::hasColumn('post', 'title')){
+            return;
+            }else{
+                Schema::table('post', function (Blueprint $table)
+                {
+                    $table->longText('title')->nullable();
+                });
+            }
+            if (Schema::hasColumn('post', 'body')){
+            return;
+            }else{
+                Schema::table('post', function (Blueprint $table)
+                {
+                    $table->longText('body')->nullable();
+                });
+            }
+            if (Schema::hasColumn('post', 'tags')){
+            return;
+            }else{
+                Schema::table('post', function (Blueprint $table)
+                {
+                    $table->string('tags')->default('N/A');
+                });
+            }
+            
+            if (Schema::hasColumn('post', 'path')){
+            return;
+            }else{
+                Schema::table('post', function (Blueprint $table)
+                {
+                    $table->string('path')->nullable();
+                });
+            }
+             
+            if (Schema::hasColumn('post', 'created_at')){
+            return;
+            }else{
+                Schema::table('post', function (Blueprint $table)
+                {
+                     $table->timestamps();
+                });
+            }
+            
+            
+            
+        }
+        
     }
 
     /**
